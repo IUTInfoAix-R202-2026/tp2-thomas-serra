152802621ee261d0edb2c092bfae6a0a0f633a7c
thomas-serra <thomas.serra.2@etu.univ-amu.fr>
2026-06-11 20:26:08 +0000

    tous les tests passent

--- src/main/java/fr/univ_amu/iut/exercice7/CercleInteractif.java
+++ src/main/java/fr/univ_amu/iut/exercice7/CercleInteractif.java
@@ -1,6 +1,8 @@
 package fr.univ_amu.iut.exercice7;
 
 import javafx.application.Application;
+import javafx.beans.binding.Bindings;
+import javafx.scene.Scene;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TextFormatter;
@@ -43,56 +45,52 @@ public class CercleInteractif extends Application {
 
   @Override
   public void start(Stage primaryStage) {
-    // TODO exercice 7 : assembler l'interface et créer les bindings.
-    //
-    // 1. Appeler ajouterPanneau() pour configurer le panneau avec le cercle.
-    // 2. Appeler ajouterSlider() pour configurer le slider en haut.
-    // 3. Appeler ajouterTextField() pour ajouter le champ de saisie en bas.
-    // 4. Appeler creerBindings() pour lier les trois contrôles.
-    // 5. Créer la Scene, l'attacher au Stage, afficher.
+    ajouterPanneau();
+    ajouterSlider();
+    ajouterTextField();
+    creerBindings();
+
+    Scene scene = new Scene(root);
+    primaryStage.setScene(scene);
+    primaryStage.show();
   }
 
   void ajouterPanneau() {
-    // TODO exercice 7 : ajouter le panneau avec le cercle.
-    //
-    // panneauCercle.getChildren().add(cercle)
-    // panneauCercle.setPrefSize(500, 500)
-    // panneauCercle.setId("panneau")
-    // cercle.setId("cercle")
-    // root.setCenter(panneauCercle)
+    panneauCercle.getChildren().add(cercle);
+    panneauCercle.setPrefSize(500, 500);
+    panneauCercle.setId("panneau");
+    cercle.setId("cercle");
+    root.setCenter(panneauCercle);
   }
 
   void ajouterSlider() {
-    // TODO exercice 7 : configurer le slider [0, 250] et le placer en haut.
-    //
-    // slider.setMin(0), slider.setMax(250)
-    // slider.setId("slider")
-    // root.setTop(slider)
+    slider.setMin(0);
+    slider.setMax(250);
+    slider.setId("slider");
+    root.setTop(slider);
   }
 
   void ajouterTextField() {
-    // TODO exercice 7 : ajouter le TextField avec un TextFormatter en bas.
-    //
-    // Le TextFormatter filtre la saisie pour n'accepter que des nombres.
-    // Ce code est fourni - ne pas modifier.
+    textField.setId("rayon");
+    textField.setTextFormatter(
+        new TextFormatter<>(
+            change -> {
+              String newText = change.getControlNewText();
+              return newText.matches("\\d*") ? change : null;
+            }));
+    root.setBottom(textField);
   }
 
   void creerBindings() {
-    // TODO exercice 7 : créer les bindings bidirectionnels.
-    //
-    // 1. Centrer le cercle dans le panneau (unidirectionnel) :
-    //    cercle.centerXProperty().bind(panneauCercle.widthProperty().divide(2))
-    //    cercle.centerYProperty().bind(panneauCercle.heightProperty().divide(2))
-    //
-    // 2. Lier le rayon du cercle au slider (bidirectionnel) :
-    //    cercle.radiusProperty().bindBidirectional(slider.valueProperty())
-    //
-    // 3. Lier le slider au TextField via NumberStringConverter :
-    //    Bindings.bindBidirectional(textField.textProperty(),
-    //        slider.valueProperty(), new NumberStringConverter())
-    //
-    // 4. Initialiser le rayon à 150 :
-    //    slider.setValue(150)
+    cercle.centerXProperty().bind(panneauCercle.widthProperty().divide(2));
+    cercle.centerYProperty().bind(panneauCercle.heightProperty().divide(2));
+
+    cercle.radiusProperty().bindBidirectional(slider.valueProperty());
+
+    Bindings.bindBidirectional(
+        textField.textProperty(), slider.valueProperty(), new NumberStringConverter());
+
+    slider.setValue(150);
   }
 
   public static void main(String[] args) {
